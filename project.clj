2b79a10264dabad1a813de25a20f002164e12996
Jon Eisen <jon@joneisen.me>
2015-12-13 01:15:57 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-node-app/lein-template "0.1.0"
+(defproject cljs-node-app/lein-template "0.1.1-SNAPSHOT"
   :description "Clojurescript Node App Leiningen Template"
   :url "https://github.com/yanatan16/cljs-node-app-template"
   :license {:name "Eclipse Public License"
